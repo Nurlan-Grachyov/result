89ed79b385dc0abd528ed4987043a5e9df7df256
Nurlan <nurlan.grachyov@mail.ru>
2024-10-24 00:11:15 +0400

    Исправил

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
         )
     )
 
-    # print(investment_bank(str_date_service, transactions, 50))
-    #
-    # print(spending_by_weekday(df_file, str_date_report))
+    print(investment_bank(str_date_service, transactions, 50))
+
+    print(spending_by_weekday(df_file, str_date_report))
